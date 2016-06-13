0bc7791d1d48c4901a2c2da75ae3163e48371c14
vi5i0n <vi5i0n@hotmail.com>
2016-06-13 14:43:26 +0800

    pep8 format

--- doubanrobot.py
+++ doubanrobot.py
@@ -3,7 +3,7 @@
 # @Date    : 2016-03-20 18:17:54
 # @Author  : Linsir (root@linsir.org)
 # @Link    : http://linsir.org
-# @Version : 0.1
+# @Version : 0.2
 
 import requests
 import requests.utils
@@ -11,26 +11,41 @@ import pickle
 import json
 import re
 import random
+import logging
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-email = 'xxx@email.com'
-password = 'your_passwd'
+email = 'username@email.com'
+password = 'password'
 cookies_file = 'cookies.txt'
- 
-class douban_robot:
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='doubanrobot.log',
+                    filemode='a')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-2s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+class DoubanRobot:
+    '''
+    A simple robot for douban.com
+    '''
     def __init__(self):
         self.email = email
         self.password = password
+        self.ck = None
         self.data = {
                 "form_email": email,
                 "form_password": password,
                 "source": "index_nav",
                 "remember": "on"
-        }  
+        }
         self.session = requests.Session()
         self.login_url = 'https://www.douban.com/accounts/login'
         self.session.headers = {
@@ -38,44 +53,63 @@ class douban_robot:
             "Origin": "https://www.douban.com",
         }
         # self.session.headers = self.headers
-        self.load_cookies()
-        self.get_ck()
+        if not self.load_cookies():
+            if self.login():
+                self.get_ck()
+        else:
+            self.get_ck()
 
     def load_cookies(self):
-        with open(cookies_file) as f:
-            self.session.cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
+        '''
+        load cookies from file.
+        '''
+        try:
+            with open(cookies_file) as f:
+                self.session.cookies = requests.utils.cookiejar_from_dict(pickle.load(f))
+            return True
+        except Exception, e:
+            logging.error('faild to load cookies from file.')
+            return False
 
     def save_cookies(self):
+        '''
+        save cookies to file.
+        '''
         with open(cookies_file, 'w') as f:
             pickle.dump(requests.utils.dict_from_cookiejar(self.session.cookies), f)
+        logging.info('save cookies to file.')
 
     def get_ck(self):
-
+        '''
+        open douban.com and then get the ck from html.
+        '''
         cookies = self.session.cookies.get_dict()
         # r = self.session.get('http://httpbin.org/get',)
         r = self.session.get('https://www.douban.com',)
         if r.cookies.get_dict():
-            print 'cookies is end of date, login agin'
+            logging.info('cookies is end of date, login again')
             self.login()
             self.get_ck()
         if cookies.has_key('ck'):
             self.ck = cookies['ck'].strip('"')
-            print "ck:%s" %self.ck
+            logging.info("ck:%s" %self.ck)
         else:
-            print 'cookies is end of date.'
+            logging.error('cannot get the ck. ')
 
     def login(self):
-        url = 'http://httpbin.org/post'
+        '''
+        login douban.com and save the cookies to file.
+        '''
+        # url = 'http://httpbin.org/post'
         r = self.session.post(self.login_url, data=self.data,)
         html =  r.text
-        # print html
         # save_html('1.html', html)
         # 验证码
         regex = r'<img id="captcha_image" src="(.+?)" alt="captcha"'
         imgurl = re.compile(regex).findall(html)
         if imgurl:
             print "The captcha_image url address is %s" %imgurl[0]
-             
+
             captcha = re.search('<input type="hidden" name="captcha-id" value="(.+?)"/>', html)
             if captcha:
                 vcode=raw_input('图片上的验证码是：')
@@ -88,13 +122,19 @@ class douban_robot:
             # save_html('2.html',r.text)
         if r.url == 'https://www.douban.com/':
             self.save_cookies()
-            print 'login successfully!'
+            logging.info('login successfully!')
         else:
+            logging.error('Faild to login, check username and password and captcha code.')
             return False
         return True
 
     def new_topic(self, group_id, title, content='Post by python'):
-
+        '''
+        use the ck pulish a new topic on the douban group.
+        '''
+        if not self.ck:
+            logging.error('ck is invalid!')
+            return False
         group_url = "https://www.douban.com/group/" + group_id
         post_url = group_url + "/new_topic"
         post_data = {
@@ -105,11 +145,18 @@ class douban_robot:
             }
         r = self.session.post(group_url, post_data)
         if r.url == group_url:
-            print 'Okay, new_topic: %s post successfully !'%title
+            logging.info('Okay, new_topic: %s post successfully !'%title)
             return True
         return False
 
-    def talk_statuses(self, content='Hello.it\'s a test message using python.'):
+    def talk_status(self, content='Hello.it\'s a test message using python.'):
+        '''
+        talk a status.
+        '''
+        if not self.ck:
+            logging.error('ck is invalid!')
+            return False
+
         post_data = {
             'ck' : self.ck,
             'comment' : content,
@@ -117,8 +164,17 @@ class douban_robot:
 
         self.session.headers["Referer"] = "https://www.douban.com/"
         r = self.session.post("https://www.douban.com/", post_data,)
+        if r.status_code == 200:
+            logging.info('Okay, talk_status: %s post successfully !'%content)
+            return True
 
     def send_mail(self, id ,content = 'Hey,Linsir !'):
+        '''
+        send a doumail to other.
+        '''
+        if not self.ck:
+            logging.error('ck is invalid!')
+            return False
 
         post_data = {
            "ck" : self.ck,
@@ -128,17 +184,25 @@ class douban_robot:
            }
         self.session.headers["Referer"] = "https://www.douban.com/doumail/write"
         r = self.session.post("https://www.douban.com/doumail/write", post_data,)
-
+        if r.status_code == 200:
+            logging.info('Okay, send_mail: To %s doumail "%s" successfully !'%(id, content))
+            return True
     def sofa(self,
             group_id,
-            content=['丫鬟命，公主心，怪不得人。', 
+            content=['丫鬟命，公主心，怪不得人。',
                     '要交流就平等交流，弄得一副跪舔样,谁还能瞧得起你？',
                     '己所欲，勿施于人..',
                     '人在做，天在看.',]
             ):
+        '''
+        Randomly select a content and reply a topic.
+        '''
+        if not self.ck:
+            logging.error('ck is invalid!')
+            return False
 
         group_url = "https://www.douban.com/group/" + group_id +"/#topics"
-        html = self.opener.open(group_url).read() 
+        html = self.opener.open(group_url).read()
         topics = re.findall(r'topic/(\d+?)/.*?class="">.*?<td nowrap="nowrap" class="">(.*?)</td>',
                     html, re.DOTALL)
 
@@ -151,8 +215,14 @@ class douban_robot:
                         "submit_btn" : "加上去"
                 }
                 self.session.post("https://www.douban.com/group/topic/" + item[0] + "/add_comment#last?", post_data)
+                if r.status_code == 200:
+                    logging.info('Okay, send_mail: To %s doumail "%s" successfully !'%(id, content))
+        return True
 
     def get_joke(self):
+        '''
+        get a joke from http://www.xiaohuayoumo.com/
+        '''
         html = self.session.get('http://www.xiaohuayoumo.com/').text
         result = re.compile(r']<a href="(.+?)">(.+?)</a></div>.+?', re.DOTALL).findall(html)
         for x in result[:1]:
@@ -164,7 +234,7 @@ class douban_robot:
             for x in result[:1]:
                 content = x[0] + x[1]
                 content = re.sub(r'</?\w+[^>]*>',' ',content)
-
+        logging.info('get a joke from http://www.xiaohuayoumo.com/')
         return title, content
 
 
@@ -173,12 +243,13 @@ def save_html(name, data):
         f.write(data)
 
 if __name__ == '__main__':
-    app = douban_robot()
+    app = DoubanRobot()
+    # app.login()
     titile, content = app.get_joke()
     # print content
     # if titile and content:
     #     print app.new_topic("cd", titile, content)
-    # app.talk_statuses()
+    app.talk_status()
     app.send_mail(63666378)
     # app.sofa("CentOS")
 
